4eec9b7d42c5f8d9549acb7974a0667af7db5d9b
AR Nadal Verma <108530394+arnadal@users.noreply.github.com>
2025-04-03 22:51:32 +0530

    Add /guidelines redirect

--- next.config.ts
+++ next.config.ts
@@ -7,6 +7,11 @@ const nextConfig: NextConfig = {
         source: '/about',
         destination: 'https://docs.google.com/document/d/1ZcuRdbz4S0YNhIP8rr2sIUP1hg2KEjjzTgnUho8ATAs/edit?usp=sharing',
         permanent: true,
+      },
+      {
+        source: '/guidelines',
+        destination: 'https://docs.google.com/document/d/1ZcuRdbz4S0YNhIP8rr2sIUP1hg2KEjjzTgnUho8ATAs/edit?usp=sharing',
+        permanent: true,
       }
     ]
   },
