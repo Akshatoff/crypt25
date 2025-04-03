dbbefbb71ffe7ad7736caf782a1ff2bf9deac6a2
AR Nadal Verma <108530394+arnadal@users.noreply.github.com>
2025-04-03 22:32:19 +0530

    Add /about redirect

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-};
+  async redirects() {
+    return [
+      {
+        source: '/about',
+        destination: 'https://docs.google.com/document/d/1ZcuRdbz4S0YNhIP8rr2sIUP1hg2KEjjzTgnUho8ATAs/edit?usp=sharing',
+        permanent: true,
+      }
+    ]
+  },
+}
 
 export default nextConfig;
